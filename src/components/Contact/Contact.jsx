820a2b37c699cb3db69bde0c4b761aa0cc375d02
Felipe Lutz <felipeclutz@gmail.com>
2025-07-24 20:45:31 -0300

    Criando modal de confirmação de envio de email

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -14,6 +14,18 @@ export const Contact = ({sectionRef3}) =>{
     const [enterprise, setEnterprise] = useState('')
     const [message, setMessage] = useState('')
 
+    const openModalMessageSuccess = () =>{
+        const modal = document.querySelector(".modal-message-success")
+
+        modal.showModal()
+    } 
+
+    const closeModalMessageSuccess = () =>{
+        const modal = document.querySelector(".modal-message-success")
+
+        modal.close()
+    }
+
     const handleSubmit = async (e) =>{
         e.preventDefault()
 
@@ -35,62 +47,65 @@ export const Contact = ({sectionRef3}) =>{
                 setPhone("")
                 setEnterprise("")
                 setMessage("")
+                openModalMessageSuccess()
             }, (err) => {
                 console.log("Erro:", err)
             })
     }
 
     return(
-        <div ref={sectionRef3} className="container-contact">
-            <div className='div-contact-info'>
-                <h2>Entre em contato</h2>
-                <h1>Vamos trabalhar juntos!</h1>
-
-                <div className="div-my-info">
-                    <p className='div-my-info-title'>Email</p>
-                    <div className='div-icon-info'>
-                        <HiOutlineMail />
-                        <p>felipeclutz@gmail.com</p>
+        <>
+            <dialog className="modal-message-success">
+                <p>E-mail enviado com sucesso</p>
+                <button onClick={() => closeModalMessageSuccess()}>Fechar</button>
+            </dialog>
+            <div ref={sectionRef3} className="container-contact">
+                <div className='div-contact-info'>
+                    <h2>Entre em contato</h2>
+                    <h1>Vamos trabalhar juntos!</h1>
+                    <div className="div-my-info">
+                        <p className='div-my-info-title'>Email</p>
+                        <div className='div-icon-info'>
+                            <HiOutlineMail />
+                            <p>felipeclutz@gmail.com</p>
+                        </div>
+                    </div>
+                    <div className="div-my-info">
+                        <p className='div-my-info-title'>Endereço</p>
+                        <div className='div-icon-info'>
+                            <SlLocationPin />
+                            <p>Petrópolis, RJ</p>
+                        </div>
                     </div>
                 </div>
-
-                <div className="div-my-info">
-                    <p className='div-my-info-title'>Endereço</p>
-                    <div className='div-icon-info'>
-                        <SlLocationPin />
-                        <p>Petrópolis, RJ</p>
+                <form className="form-contact" onSubmit={(e) => handleSubmit(e)}>
+                    <div className='div-form-contact'>
+                        <div className='div-input'>
+                            <label htmlFor="input-name">Nome</label>
+                            <input type="text" id="input-name" value={name} placeholder="Nome" required onChange={(e) => setName(e.target.value)}/>
+                        </div>
+                        <div className='div-input'>
+                            <label htmlFor="input-email">Email</label>
+                            <input type="email" id="input-email" value={email} placeholder="Seu melhor email" required onChange={(e) => setEmail(e.target.value)}/>
+                        </div>
+                        <div className='div-input'>
+                            <label htmlFor="input-number">Telefone (opcional)</label>
+                            <input type="tel" value={phone} placeholder="Seu melhor número" id="input-number" onChange={(e) => setPhone(e.target.value)}/>
+                        </div>
+                        <div className='div-input'>
+                            <label htmlFor="input-company">Empresa (opcional)</label>
+                            <input type="text" value={enterprise} placeholder="Empresa" id="input-company" onChange={(e) => setEnterprise(e.target.value)}/>
+                        </div>
+                        <div className='div-input'>
+                            <label htmlFor="input-message">Mensagem</label>
+                            <textarea id="input-message" value={message} placeholder="Sua mensagem" onChange={(e) => setMessage(e.target.value)} required/>
+                        </div>
                     </div>
-                </div>
+                    <div>
+                        <button className='btn-send-form' type='submit'>Enviar</button>
+                    </div>
+                </form>
             </div>
-
-            <form className="form-contact" onSubmit={(e) => handleSubmit(e)}>
-                <div className='div-form-contact'>
-                    <div className='div-input'>
-                        <label htmlFor="input-name">Nome</label>
-                        <input type="text" id="input-name" value={name} placeholder="Nome" required onChange={(e) => setName(e.target.value)}/>
-                    </div>
-                    <div className='div-input'>
-                        <label htmlFor="input-email">Email</label>
-                        <input type="email" id="input-email" value={email} placeholder="Seu melhor email" required onChange={(e) => setEmail(e.target.value)}/>
-                    </div>
-                    <div className='div-input'>
-                        <label htmlFor="input-number">Telefone (opcional)</label>
-                        <input type="tel" value={phone} placeholder="Seu melhor número" id="input-number" onChange={(e) => setPhone(e.target.value)}/>
-                    </div>
-                    <div className='div-input'>
-                        <label htmlFor="input-company">Empresa (opcional)</label>
-                        <input type="text" value={enterprise} placeholder="Empresa" id="input-company" onChange={(e) => setEnterprise(e.target.value)}/>
-                    </div>
-                    <div className='div-input'>
-                        <label htmlFor="input-message">Mensagem</label>
-                        <textarea id="input-message" value={message} placeholder="Sua mensagem" onChange={(e) => setMessage(e.target.value)} required/>
-                    </div>
-                </div>
-
-                <div>
-                    <button className='btn-send-form' type='submit'>Enviar</button>
-                </div>
-            </form>
-        </div>
+        </>
     )
 }
